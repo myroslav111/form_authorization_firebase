b4830c0e0e763cdbc78ef80f67bbf830002b216b
myroslav <myrkozar@gmail.com>
2022-09-18 17:29:21 +0300

    gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB_Xiu7kQjscqKcaNNxQQEdS_zboEd3vxM',
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // apiKey: 'AIzaSyB_Xiu7kQjscqKcaNNxQQEdS_zboEd3vxM',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
